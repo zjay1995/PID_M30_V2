32c4cec5e936d869eba23e5e4d169e9f335b3042
Zhijie Wang <zjwa127@gmail.com>
2023-05-19 07:26:04 -0700

    Update a_PID_M30_NH3.ino

--- a_PID_M30_NH3/a_PID_M30_NH3.ino
+++ a_PID_M30_NH3/a_PID_M30_NH3.ino
@@ -115,7 +115,7 @@ void setup() {
   // Gas Manager
   g_gasManager.setConfigurationManager(&g_configurationManager);
 
-  g_gasManager.addGas(Gas("NH3", 1.0));
+  g_gasManager.addGas(Gas("CO", 1.0));
   g_gasManager.addGas(Gas("O2", 1.0));
   //g_gasManager.addGas(Gas("H2S", 1.0));
   //g_gasManager.addGas(Gas("CO", 5.73));
@@ -182,7 +182,7 @@ void setup() {
   vector<Menu*> gasMenus;
 
 
-  gasMenus.push_back(new GasMenuItem("NH3", "LIBRARY",  0, &g_gasManager, gasMenuRenderer));
+  gasMenus.push_back(new GasMenuItem("CO", "LIBRARY",  0, &g_gasManager, gasMenuRenderer));
   gasMenus.push_back(new GasMenuItem("O2", "LIBRARY", 1, &g_gasManager, gasMenuRenderer));
   //gasMenus.push_back(new GasMenuItem("H2S", "LIBRARY", 2, &g_gasManager, gasMenuRenderer));
   //gasMenus.push_back(new GasMenuItem("CO", "LIBRARY",  3, &g_gasManager, gasMenuRenderer));
